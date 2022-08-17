9b432fa4b5a177706abbefdbe0a40990cfae31a9
Satweek Tripathy <smoxics@gmail.com>
2022-08-17 13:39:10 +0530

    deleted a garbage file

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-from urllib import request, parse
-
-def parseUrl(url: str):
-    res = parse.urlsplit(url)
-    print(res)
-    
-    
-parseUrl("https://open.spotify.com/playlist/2M6Y13hSQkCwodeOUieUom")
